83246875ce72e9b157514d09d883180fba5e2596
Evelyn <evelyn@e-s.me.uk>
2019-03-08 17:50:28 +0000

    Add invocation modes and roots

--- main/events.py
+++ main/events.py
@@ -1,11 +1,12 @@
 class EventTree:
-    def __init__(self, name):
+    def __init__(self, name, root=None):
+        self._root = root or self
         self._trees = {}
         self._hooks = set()
 
     def _guarantee_tree(self, tree_name):
         if tree_name not in self._trees:
-            self._trees[tree_name] = EventTree(tree_name)
+            self._trees[tree_name] = EventTree(tree_name, self)
         return self._trees[tree_name]
 
     def single(self, event_name):
@@ -17,16 +18,16 @@ class EventTree:
     def __getitem__(self, event_name):
         return self.single(event_name)
 
-    def hook(self, module_instance, function_reference):
+    def hook(self, module_instance, function_reference, invocation_mode=0, completion_event=None):
         self._hooks.add((module_instance, function_reference))
 
-    def invoke(self, event_name, event):
+    def invoke(self, event_name, event, force_invocation_mode=0):
         current_tree = self
         for part in event_name.split("/"):
             current_tree = current_tree._guarantee_tree(part)
-            current_tree._invoke(event)
+            current_tree._invoke(event, force_invocation_mode)
 
-    def _invoke(self, event):
+    def _invoke(self, event, force_invocation_mode=0):
         for module_instance, function_reference in self._hooks:
             function_reference(module_instance, event)
 
