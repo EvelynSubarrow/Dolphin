1fbb054d96291288d98b57e40e2b022ea9f01844
Evelyn <evelyn@e-s.me.uk>
2019-06-12 10:25:56 +0100

    Use module loader to load module loader

--- dolphin.py
+++ dolphin.py
@@ -3,6 +3,7 @@
 import importlib.util, importlib.abc, typing, sys
 
 from main import events
+from module import module_loader
 
 class Main:
     def __init__(self):
@@ -15,21 +16,8 @@ class Main:
         self.events["start"]()
 
     def load_module_loader(self):
-        module_spec = importlib.util.spec_from_file_location("module/module_loader:0000", "module/module_loader.py")
-        module = importlib.util.module_from_spec(module_spec)
-
-        loader = typing.cast(importlib.abc.Loader, module_spec.loader)
-        loader.exec_module(module)
-
-        module.instance = module.Module(self)
-        module.id = 0
-        module.enabled = True
-
-        module.instance.on_load(self)
-        module.instance.on_enable(self)
-
-        module.instance.modules["module_loader"] = module
-
+        initial_loader = module_loader.Module(self)
+        initial_loader.load_module("module_loader")
 
 if __name__=="__main__":
     main = Main().main()
