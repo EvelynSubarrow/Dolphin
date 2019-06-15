9a7aad751a95cae1e3e3fb8d84e7c5c3c2aa994f
Evelyn <evelyn@e-s.me.uk>
2019-06-15 15:15:55 +0100

    Add invocations for config entries

--- module/config.py
+++ module/config.py
@@ -1,5 +1,20 @@
+import os
+from collections import OrderedDict
+
 from main.module import Module
 
 class Module(Module):
+    def __init__(self, main):
+        self._main = main
+
     def on_enable(self, main):
         main.config = self
+
+    # Individual items, default response hooks should be registered north of 0
+    def __getitem__(self, key):
+        main.events["config/collect/{}".format(key)](key)
+
+    # This is for the likes of irc/server/*, defaults shouldn't be provided or
+    # requested
+    def get_keys(self, key):
+        main.events["config/keys/{}".format(key)](key)
