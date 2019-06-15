ed6c61a306a15b0a6de48dbe6cc4d2b722fd0b75
Evelyn <evelyn@e-s.me.uk>
2019-06-15 15:19:48 +0100

    config: properly reference main

--- module/config.py
+++ module/config.py
@@ -2,16 +2,16 @@ from main.module import Module
 
 class Module(Module):
     def __init__(self, main):
-        self._main = main
+        self.main = main
 
     def on_enable(self, main):
         main.config = self
 
     # Individual items, default response hooks should be registered north of 0
     def __getitem__(self, key):
-        main.events["config/collect/{}".format(key)](key)
+        self.main.events["config/collect/{}".format(key)](key)
 
     # This is for the likes of irc/server/*, defaults shouldn't be provided or
     # requested
     def get_keys(self, key):
-        main.events["config/keys/{}".format(key)](key)
+        self.main.events["config/keys/{}".format(key)](key)
