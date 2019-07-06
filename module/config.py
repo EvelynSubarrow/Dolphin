0f1961e4a096f8fa5ea9b27350f017e1f272df97
Evelyn <evelyn@e-s.me.uk>
2019-07-06 15:36:14 +0100

    config: return things, accomodate request for nonexistent key

--- module/config.py
+++ module/config.py
@@ -9,9 +9,14 @@ class Module(Module):
 
     # Individual items, default response hooks should be registered north of 0
     def __getitem__(self, key):
-        self.main.events["config/collect/{}".format(key)](key)
+        responses = self.main.events["config/collect/{}".format(key)](key)
+        if len(responses):
+            return responses[0]
+        else:
+            return None
 
     # This is for the likes of irc/server/*, defaults shouldn't be provided or
     # requested
+    # TODO: condense all keys across all responses
     def get_keys(self, key):
-        self.main.events["config/keys/{}".format(key)](key)
+        return self.main.events["config/keys/{}".format(key)](key)[0]
