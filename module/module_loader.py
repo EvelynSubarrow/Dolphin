9e4b20b4ccca2d47543032289192a48656ec9989
Evelyn <evelyn@e-s.me.uk>
2019-07-26 16:36:32 +0100

    module loader - short slugs for events, load modules listed in module/module_loader/modules

--- module/module_loader.py
+++ module/module_loader.py
@@ -13,6 +13,7 @@ class Module(Module):
 
     def on_enable(self, main):
         main.modules = self
+        main.events["start"].hook(self.initial_load)
 
     def enable_module(self, name):
         self.modules[name].enabled = True
@@ -24,12 +25,16 @@ class Module(Module):
         self.modules[name].instance.on_disable(self.main)
         self.main.events["module_loader/module/disable/{}".format(name)](name=name)
 
+    def initial_load(self, event):
+        for module_name in self.main.config.get_keys("module/module_loader/modules"):
+            self.load_module(module_name)
+
     def load_module(self, name):
         self.current_index += 1
-        self._load_module(name, "module/{}:{:04X}".format(name, self.current_index), "module/{}.py".format(name))
+        self._load_module(name, "{}__{}".format(name, self.current_index), "module/{}.py".format(name))
 
     def _load_module(self, short_name, name, path):
-        if any(self.main.events["module_loader/module/preload/{}".format(name)](name=name)):
+        if any(self.main.events["module_loader/module/preload/{}".format(short_name)](name=name, id=self.current_index)):
             return
 
         module_spec = importlib.util.spec_from_file_location(name, path)
@@ -46,7 +51,7 @@ class Module(Module):
 
         module.instance.on_load(self.main)
         self.enable_module(short_name)
-        self.main.events["module_loader/module/load/{}".format(name)](name=name)
+        self.main.events["module_loader/module/load/{}".format(short_name)](name=name)
 
 
     def unload_module(self, name):
