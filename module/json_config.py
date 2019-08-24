87548b2771860cfa0fa337ef77425c343146e54b
Evelyn <evelyn@e-s.me.uk>
2019-08-24 21:08:43 +0100

    json_config - ignore config path preceding .

--- module/json_config.py
+++ module/json_config.py
@@ -22,6 +22,7 @@ class Module(Module):
             for filename in sorted(files):
                 if not filename.startswith(".") and filename.endswith(".json"):
                     config_path = root.split("/")[1:] + [filename.rstrip(".json")]
+                    config_path = [a.split(".", 1)[-1] for a in config_path]
                     with open(root + "/" + filename) as f:
                         self.fold(out, json.load(f), config_path)
         return out
